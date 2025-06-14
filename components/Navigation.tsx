d139cc883f21f1a1e5f18c66ee17806518d78e88
Blake <bgeard@wgu.edu>
2025-06-13 20:19:46 -0400

    fixes gray bg color for nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -14,7 +14,7 @@ const Navigation = () => {
   ];
 
   return (
-    <nav className="fixed top-0 w-full bg-[#0c0c12] border-b border-gray-800 z-50 shadow-sm">
+    <nav className="fixed top-0 w-full bg-gray-950 border-b border-gray-800 z-50 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
